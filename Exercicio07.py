f4381b65134934cdfe59d7e590d4a6b3378c697d
guilhermepeppi <guilhermepeppi@gmail.com>
2024-03-11 08:46:43 -0300

    Update

---
+++ Exercicio07.py
@@ -0,0 +1,6 @@
+primeira_nota = float(input('Primeira nota do aluno: '))
+segunda_nota = float(input('Segunda nota do aluno: '))
+
+media = (primeira_nota + segunda_nota) / 2
+
+print(f'A média entre {primeira_nota} e {segunda_nota} é igual a {media}')
