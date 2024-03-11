bb1925e5b86c9b48a9b1d7a1a6bb1f9eea00fb40
guilhermepeppi <guilhermepeppi@gmail.com>
2024-03-11 08:36:16 -0300

    Update

---
+++ Exercicio06.py
@@ -0,0 +1,9 @@
+import math
+
+num = int(input('Digite um número: '))
+
+raiz_quadrada = round(math.sqrt(num), 2)
+
+print(f'O dobro de {num} vale {num*2}')
+print(f'0 triplo de {num} vale {num*3}.')
+print(f'A raiz quadrada de {num} é igual a {raiz_quadrada}.')
