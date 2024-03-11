a36358507fe877050219d7198a707ca93f8f033f
guilhermepeppi <guilhermepeppi@gmail.com>
2024-03-11 08:02:54 -0300

    Update

---
+++ Exercicio03.py
@@ -0,0 +1,7 @@
+valor_um = int(input('Digite o primeiro valor: '))
+
+valor_dois = int(input('Digite o segundo valor: '))
+
+valor_final = valor_um + valor_dois
+
+print(f'A soma de {valor_um} + {valor_dois} é: {valor_final}')
