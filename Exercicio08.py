64d57dff5c35abbd63f4318f724328a48aa2e462
guilhermepeppi <guilhermepeppi@gmail.com>
2024-03-11 08:57:17 -0300

    Update

---
+++ Exercicio08.py
@@ -0,0 +1,8 @@
+distancia = float(input('Uma distância em metros: '))
+print(f'A medida de {distancia} corresponde a: ')
+print(f'{distancia/1000}km ')
+print(f'{distancia/100}hm ')
+print(f'{distancia/10}dam ')
+print(f'{distancia*10}dm')
+print(f'{distancia*10}cm ')
+print(f'{distancia*1000}mm ', format)
