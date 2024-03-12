e751dc6a6c01d08ef64876c327d6b5734556f643
guilhermepeppi <guilhermepeppi@gmail.com>
2024-03-12 08:17:56 -0300

    Update

---
+++ Exercicio23.py
@@ -0,0 +1,9 @@
+numero = input('Informe um número: ')
+
+print(f'Analisando o numero {numero}')
+
+
+print(f'Unidade: {numero[3]}')
+print(f'Dezena: {numero[2]}')
+print(f'Centena: {numero[1]}')
+print(f'Milhar: {numero[0]}')
