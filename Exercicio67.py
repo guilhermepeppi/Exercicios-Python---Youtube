2a35f6d3eb4829c0435d69f783b71383dba63fa6
guilhermepeppi <guilhermepeppi@gmail.com>
2024-03-18 08:53:00 -0300

    Update

---
+++ Exercicio67.py
@@ -0,0 +1,3 @@
+# Faça um programa que mostre a tabuada de vários números,
+# um de cada vez, para cada valor digitado palo usuário.
+# O programa será interrompido quando o númaro solicitado for negativo.
