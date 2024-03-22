1a7bd35638405d1373b162cd6a170c9fe3d2dffc
guilhermepeppi <guilhermepeppi@gmail.com>
2024-03-22 14:18:37 -0300

    Update

---
+++ Exercicio100.py
@@ -0,0 +1,5 @@
+# Faça um programa que tenha uma lista chamada números e
+# duas funções chamadas sorteia() e somaPar().
+# A primaira função vai sortear 5 números a vai colocá-los dentro da lista
+# e a segunda função vai mostrar a soma entre todos
+# os valores PARES sortcados pela função anterior.
