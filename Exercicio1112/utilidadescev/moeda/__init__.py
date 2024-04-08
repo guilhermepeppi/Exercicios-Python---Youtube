3b8a12789729cb9dc49950025f896011f8ef1662
guilhermepeppi <guilhermepeppi@gmail.com>
2024-04-08 19:01:05 -0300

    Upload

--- Exercicio1112/utilidadescev/moeda/__init__.py
+++ Exercicio1112/utilidadescev/moeda/__init__.py
@@ -1,6 +1,7 @@
-# Crie um pacote chamado utilidadesCeV que tenha dois módulos internos chamados moeda e dado.
-# Transfira todas as funções utilizadas nos desafios 107, 108 e 109
-# para o primeiro pacote e mantenha tudo funcionando.
+# Dentro do pacote utilidadesCeV que criamos no desafio 111, temos um módulo
+# chamado dado. Crie uma função chamada leiaDinheiro() que seja capaz de
+# funcionar como a função imputa(), mas com uma validação de dados para aceitar
+# apenas valores que seja monetários.
 # ---
 # Adapte o código do desafio #107,
 # criando uma função adicional chamada moeda()
