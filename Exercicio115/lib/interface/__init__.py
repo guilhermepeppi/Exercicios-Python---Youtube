04e8de3d00ab6224135af39c81e3829b0173ce8f
guilhermepeppi <guilhermepeppi@gmail.com>
2024-04-09 09:57:26 -0300

    Upload

---
+++ Exercicio115/lib/interface/__init__.py
@@ -0,0 +1,12 @@
+def linha(tamanho=42):
+    return '-' * tamanho
+
+
+def cabecalho(txt):
+    print(linha())
+    print(txt)
+    print(linha())
+
+
+def menu(lista):
+    print(lista)
---
+++ Exercicio115/sistema.py
@@ -0,0 +1,10 @@
+# Vamos criar um menu em Python, usando modularização.
+# Crie um pequeno sistema modularizado que permita cadastrar passoas pelo seu
+# nome e idade em um arquivo de texto simplas. O sistema só vai ter 2 opcoes:
+# 1) Cadastrar uma nova passoa;
+# 2) Listar todas as pessoas cadastradas.
+
+import lib.interface
+
+cabecalho('SISTEMA ARQUIVO')
+menu(['opcao1', 'opcao2', 'opcao3'])
